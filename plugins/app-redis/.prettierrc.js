44ac75cb0a998d1e46166af53fc776f6720284a6
Jack Westbrook <jack.westbrook@gmail.com>
2022-07-25 16:17:30 +0200

    chore(redis-app): delete old configs, update configs

--- plugins/app-redis/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  useTabs: false,
-  tabWidth: 2,
-  semi: true,
-  bracketSpacing: true,
-  arrowParens: 'always',
-  ...require('./node_modules/@grafana/toolkit/src/config/prettier.plugin.config.json'),
-};
